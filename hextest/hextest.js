57bc6f5a282dad266c795da10f5162ba483e25c7
Aren Davey <adavey@andrew.cmu.edu>
2019-08-15 22:11:39 -0400

    swapped order of params in includeHex and getHex. I may have screwed up my axis so committing before I meddle

--- hextest/hextest.js
+++ hextest/hextest.js
@@ -1,6 +1,6 @@
 var radius = 5; //radius of hex grid
 var size; //size of hexes
-var originPixel; //very center of the board
+var originHex; //very center of the board
 var hexes = [];
 var mainLayout;
 
@@ -11,7 +11,7 @@ function setup()
   originPixel = Point(windowWidth/2, windowHeight/2)
   mainLayout = Layout(pointyOrient, size, originPixel)
   generateBoard(radius, hexes);
-  center = Hex(0,0,0);
+  originHex = Hex(0,0,0);
 }
 
 function draw()
@@ -24,15 +24,17 @@ function draw()
   {
     drawHex(mainLayout, hexes[i], '#BE73B2');
   }
-  //testFuncAll();
+  testFuncAll();
 }
 
 
 //function test holder. you can comment the ones you want to see in and vice versa.
 function testFuncAll()
 {
-  testIsEquals();
-  testHexArit();
+  //testIsEquals();
+  //testHexArit();
+  //testHexOverlap();
+  //testDiag();
 
 }
 function testIsEquals()
@@ -44,4 +46,14 @@ function testIsEquals()
 function testHexArit()
 {
   //var hexa =
+}
+function testHexOverlap()
+{
+
+}
+function testDiag()
+{
+  drawHex(mainLayout, originHex, '#19053A');
+  var diag0 = getDiagonal(originHex, 0);
+  drawHex(mainLayout, Hex(0,-1,1), '#684F91');
 }
