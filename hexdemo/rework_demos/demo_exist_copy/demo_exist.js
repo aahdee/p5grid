566d81c7b6776141c6a4760272e6f4286b7797eb
Aren Davey <adavey21@gmail.com>
2026-02-04 11:56:03 -0500

    sucesssful tri grid

--- hexdemo/rework_demos/demo_exist_copy/demo_exist.js
+++ hexdemo/rework_demos/demo_exist_copy/demo_exist.js
@@ -1,4 +1,4 @@
-let boardRadius = 2;
+let boardRadius = 4;
 let hSize;
 let tSize;
 
@@ -6,7 +6,7 @@ function setup()
 {
   createCanvas(windowWidth, windowHeight);
   hSize = new hexSize(30,30);
-  tSize = new triSize(30,30);
+  tSize = new triSize(30,20);
   background(25);
   angleMode(DEGREES);
   grid1 = new HexBoard(HEXPOINTYORIENTATION, hSize, boardRadius);
@@ -21,8 +21,25 @@ function draw()
   push();
   translate(width/2, height/2);
   fill("#42002b");
-  grid1.drawBoard();
+  // noFill()
+  // grid1.drawBoard();
   grid2.drawBoardDebug();
+  for(let i = 0; i < grid2.tris.length; i++){
+    let tri = grid2.tris[i];
+    
+
+    // if (tri.q == 0 && tri.r == 0 && tri.s == 0){
+    //   // hex.draw
+    //   push();
+    //   fill("red");
+    //   circle(grid2.triToScreen(tri).x,grid2.triToScreen(tri).y,10);
+    //   pop();
+    // }
+    // else{
+    //   circle(grid2.triToScreen(tri).x,grid2.triToScreen(tri).y,10)
+    // }
+  }
+
   // let h = grid1.hexes[14]
   // fill('red')
   // grid1.drawHex(h)
